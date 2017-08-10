55bcad489911552d2961e1696ddedcb727de2822
rafaelmirandam <rcsilva12@gmail.com>
2017-08-10 11:52:18 -0300

    commit3

--- forca/src/game.rb
+++
@@ -1,8 +0,0 @@
-
-class Game
-    attr_accessor :output
-
-    def start
-        self.output = 'Bem vindo ao jogo da forca!'
-    end
-end
--- soma/features/step_definitions/soma_steps.rb
+++
@@ -1,9 +0,0 @@
-#encoding:utf-8
-
-Quando(/^faço a soma de (\d+) \+ (\d+)$/) do |valor1, valor2|
-  @soma = valor1.to_i + valor2.to_i
-end
-
-Então(/^o resultado é (\d+)$/) do |resultado|
-  expect(@soma).to eql resultado.to_i
-end
